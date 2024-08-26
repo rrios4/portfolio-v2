c253891290a06c67fc815a5beb441d87a6433ccd
Rogelio Rios <rogeliorios348@gmail.com>
2024-08-25 21:28:36 -0500

    Updated yarn.lock

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,32 +1,72 @@
 import React from "react";
 
-const WorkCard = ({ img, name, description, onClick, date, repoURL, techIcons, active }) => {
+const WorkCard = ({
+  img,
+  name,
+  description,
+  onClick,
+  date,
+  repoURL,
+  techIcons,
+  active,
+}) => {
   return (
-    <div
-      className="overflow-hidden rounded-lg p-4 mob:p-2 laptop:p-4 first:ml-0">
+    <div className="overflow-hidden rounded-lg p-4 mob:p-2 laptop:p-4 first:ml-0">
       <div
         className="overflow-hidden cursor-pointer rounded-lg transition-all ease-out duration-300 hover:scale-95 mob:h-48"
         style={{ height: "400px" }}
-        onClick={onClick}>
-        <img alt={name} className="h-full w-full object-cover" src={img}></img>
+        onClick={onClick}
+      >
+        <img alt={name} className="h-full w-full object-cover" src={img} />
       </div>
       <div className="flex">
         <h1 className="mt-5 text-3xl font-medium">
           {name ? name : "Project Name"}
         </h1>
-        {active === true ? <div className="flex justify-center mt-[20px] mx-[1rem] w-[50px] h-[30px] bg-green-500 rounded-full"><h1 className="text-white m-1">Live</h1></div> : "" }
+        {active === true ? (
+          <div className="flex justify-center mt-[20px] mx-[1rem] w-[50px] h-[30px] bg-green-500 rounded-full">
+            <h1 className="text-white m-1">Live</h1>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
-      <h2 className="text-lg opacity-70">
-        {date ? date : "Project Date"}
-      </h2>
+      <h2 className="text-lg opacity-70">{date ? date : "Project Date"}</h2>
       <div className="flex my-1 space-x-[1rem] w-[40px] h-[35px]">
-          {techIcons?.map((icon, index) => (<>{icon ? <img key={index} className="w-[35px] rounded-full p-1" src={icon.url}/> : ""}</>))}
+        {techIcons?.map((icon, index) => (
+          <>
+            {icon ? (
+              <img
+                key={index}
+                className="w-[35px] rounded-full p-1"
+                src={icon.url}
+              />
+            ) : (
+              ""
+            )}
+          </>
+        ))}
       </div>
       <h2 className="text-xl opacity-50">
         {description ? description : "Description"}
       </h2>
       <div>
-        {repoURL ? <button onClick={() => window.open(repoURL)} className="my-[1rem] mr-[1rem] p-[1rem] rounded-xl bg-gray-100 hover:bg-gray-200">GitHub Repo</button> : <button disabled onClick={() => window.open(repoURL)} className="my-[1rem] mr-[1rem] p-[1rem] rounded-xl bg-gray-100 hover:bg-gray-200 border-1">Source Code Unavailable</button>}
+        {repoURL ? (
+          <button
+            onClick={() => window.open(repoURL)}
+            className="my-[1rem] mr-[1rem] p-[1rem] rounded-xl bg-gray-100 hover:bg-gray-200"
+          >
+            GitHub Repo
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => window.open(repoURL)}
+            className="my-[1rem] mr-[1rem] p-[1rem] rounded-xl bg-gray-100 hover:bg-gray-200 border-1"
+          >
+            Source Code Unavailable
+          </button>
+        )}
       </div>
       {/* <button onClick={repoURL} className="my-[1rem] mr-[1rem] p-[1rem] rounded-xl bg-yellow-400 hover:bg-yellow-300">Source Code</button>
       <button disabled className="hidden my-[1rem] p-[1rem] rounded-xl bg-yellow-400 text-black">Live Demo</button> */}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import Link from "next/link";
-import Head from 'next/head'
+import Head from "next/head";
 import Header from "../components/Header";
 import ServiceCard from "../components/ServiceCard";
 import Socials from "../components/Socials";
@@ -49,21 +49,23 @@ export default function Home() {
       </div>
       <div
         className="mt-40 mob:mt-10 laptop:mt-40 mob:p-2 laptop:p-0"
-        ref={workRef}>
+        ref={workRef}
+      >
         <h1 className="text-2xl text-bold">Work.</h1>
         <div className="mt-10 mob:mt-5 laptop:mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-4">
           {data.projects.map((project, index) => (
-            <WorkCard
-              key={index}
-              img={project.imageSrc}
-              name={project.title}
-              description={project.description}
-              date={project.date}
-              repoURL={project.repoURL}
-              techIcons={project.techIcons}
-              active={project.active}
-              onClick={() => window.open(project.url)}
-            />
+            <React.Fragment key={index}>
+              <WorkCard
+                img={project.imageSrc}
+                name={project.title}
+                description={project.description}
+                date={project.date}
+                repoURL={project.repoURL}
+                techIcons={project.techIcons}
+                active={project.active}
+                onClick={() => window.open(project.url)}
+              />
+            </React.Fragment>
           ))}
         </div>
       </div>
@@ -71,20 +73,27 @@ export default function Home() {
         <h1 className="text-2xl text-bold">Services.</h1>
         <div className="mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-6">
           {data.services.map((service, index) => (
-            <ServiceCard
-              key={index}
-              name={service.title}
-              description={service.description}
-            />
+            <React.Fragment key={index}>
+              <ServiceCard
+                key={index}
+                name={service.title}
+                description={service.description}
+              />
+            </React.Fragment>
           ))}
         </div>
       </div>
       <div
         className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0"
-        ref={aboutRef}>
+        ref={aboutRef}
+      >
         <h1 className="text-2xl text-bold">About.</h1>
-        <p className="m-5 mob:m-0 laptop:m-5 mob:mt-2 laptop:ml-0 ml-0 text-3xl mob:text-xl laptop:text2xl w-3/5 mob:w-full laptop:w-3/5">{data.aboutpara[0].paragraph}</p>
-        <p className="m-5 mob:m-0 laptop:m-5 mob:mt-2 laptop:ml-0 ml-0 text-3xl mob:text-xl laptop:text2xl w-3/5 mob:w-full laptop:w-3/5">{data.aboutpara[1].paragraph}</p>
+        <p className="m-5 mob:m-0 laptop:m-5 mob:mt-2 laptop:ml-0 ml-0 text-3xl mob:text-xl laptop:text2xl w-3/5 mob:w-full laptop:w-3/5">
+          {data.aboutpara[0].paragraph}
+        </p>
+        <p className="m-5 mob:m-0 laptop:m-5 mob:mt-2 laptop:ml-0 ml-0 text-3xl mob:text-xl laptop:text2xl w-3/5 mob:w-full laptop:w-3/5">
+          {data.aboutpara[1].paragraph}
+        </p>
       </div>
       <div className="mt-40 mob:mt-5 laptop:mt-40 mob:p-2 laptop:p-0">
         <h1 className="text-2xl text-bold">Contact.</h1>
@@ -95,7 +104,9 @@ export default function Home() {
       <h1 className="text-sm text-bold mt-10 mob:mt-2 laptop:mt-10 mob:p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="https://github.com/chetanverma16/react-portfolio-template">
-          <a className="underline underline-offset-1">Chetan Verma & Rogelio Rios</a>
+          <a className="underline underline-offset-1">
+            Chetan Verma & Rogelio Rios
+          </a>
         </Link>
       </h1>
     </div>
